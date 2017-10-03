939b9a7fc2aa043762cd17a857c809d671a84fcc
oscar0812 <oscar0812torres@gmail.com>
2017-10-03 09:28:05 -0500

    All code related files must implement this interface

---
+++ src/com/bittle/java2smali/util/files/CodeFile.java
@@ -0,0 +1,9 @@
+package com.bittle.java2smali.util.files;
+
+// a file that contains code must implement this interface
+public interface CodeFile{
+
+    String getPackageName();
+
+    String getPackagePath();
+}
